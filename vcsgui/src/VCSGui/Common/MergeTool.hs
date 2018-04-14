a7b7cf93cbd3bbab6b324f15987be1f02b20d50e
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2018-04-15 03:00:38 +1200

    Add Aeson instances

--- vcsgui/src/VCSGui/Common/MergeTool.hs
+++ vcsgui/src/VCSGui/Common/MergeTool.hs
@@ -11,18 +11,23 @@
 -- | Types associated with resolving conflicts with a 'Mergetool'.
 --
 -----------------------------------------------------------------------------
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 module VCSGui.Common.MergeTool (
     MergeTool (..)
     , MergeToolSetter
 
 ) where
 
+import GHC.Generics (Generic)
+import Data.Aeson (FromJSON, ToJSON)
 
 -- | Representation of a mergetool, e.g. kdiff3
-data MergeTool = MergeTool {
+newtype MergeTool = MergeTool {
     fullPath :: FilePath
-    } deriving (Show, Read)
+    } deriving (Show, Read, Generic)
+
+instance ToJSON MergeTool
+instance FromJSON MergeTool
 
 -- | Fn to set a 'MergeTool'.
 type MergeToolSetter = MergeTool -> IO()
